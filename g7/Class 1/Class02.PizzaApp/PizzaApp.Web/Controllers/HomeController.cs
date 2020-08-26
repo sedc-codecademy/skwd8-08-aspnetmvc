6dfa29c9a340cb069830a8628825a434561f1053
ivletest <karakan@protonmail.com>
2020-08-26 21:05:30 +0200

    G7 mvc-02

---
+++ g7/Class 1/Class02.PizzaApp/PizzaApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaApp.Web.Controllers
+{
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        [HttpGet("Index")]
+        public IActionResult IndexPage()
+        {
+            return View("Home");
+        }
+
+        [HttpGet("About")]
+        public IActionResult AboutPage()
+        {
+            return View("About");
+        }
+
+        [HttpGet("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
