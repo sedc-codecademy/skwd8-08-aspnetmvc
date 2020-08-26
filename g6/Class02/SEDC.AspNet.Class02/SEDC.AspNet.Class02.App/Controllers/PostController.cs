82add99ff3d404a9134fcca53683755b4430b56c
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-08-26 21:09:47 +0200

    added class 02

---
+++ g6/Class02/SEDC.AspNet.Class02/SEDC.AspNet.Class02.App/Controllers/PostController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.AspNet.Class02.App.Models;
+
+namespace SEDC.AspNet.Class02.App.Controllers
+{
+    //[Route("[controller]/[action]")]
+    [Route("post-sedc")]
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // httpverbs GET,POST,PUT,DELETE,PATCH,OPTION
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePost(TestModel request)
+        {
+            return Json(request);
+        }
+
+        [HttpGet("read-post/{id:int}")]
+        //[Route("read-post/{id:int}")]
+        public IActionResult ReadPostById(int id)
+        {
+            var testModel = new
+            {
+                Name = "Trajan",
+                Id = id
+            };
+            return Json(testModel);
+        }
+
+        [HttpGet("read-post/{id:alpha}")]
+        //[Route("read-post/{id:alpha}")]
+        public IActionResult ReadPostByName(string id)
+        {
+            var testModel = new
+            {
+                Name = "Trajan_NamePost",
+                Id = id
+            };
+            return Json(testModel);
+        }
+    }
+}
+
+
+// Order controler
+// 2 actions
+// get order by id - integer
+// get order by id - string
+// conventional routing
+// atribute routing
---
+++ g6/Class02/SEDC.AspNet.Class02/SEDC.AspNet.Class02.App/Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.AspNet.Class02.App.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
