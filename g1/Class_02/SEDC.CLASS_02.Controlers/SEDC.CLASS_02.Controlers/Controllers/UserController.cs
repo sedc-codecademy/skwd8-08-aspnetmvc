2971f45223eddae0d59f40d2d0397c3f965b5e1b
Goce Kabov <Goce.Kabov@seavus.com>
2020-08-27 22:53:23 +0200

    added class 02

---
+++ g1/Class_02/SEDC.CLASS_02.Controlers/SEDC.CLASS_02.Controlers/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.CLASS_02.Controlers.Models;
+
+namespace SEDC.CLASS_02.Controlers.Controllers
+{
+    [Route("users")]
+    public class UserController : Controller
+    {
+        
+        //[HttpPost]
+        [HttpGet]
+        [Route("Start")]
+        public IActionResult Index() // localhost:[PORT]/user/index
+        {
+            var users = StaticDb.Users;
+            return View(users);
+        }
+
+        [Route("details/{id:int}")]
+        public IActionResult UserDetails(int id)
+        {
+            UserModel model = StaticDb.Users.SingleOrDefault(user => user.Id == id);
+            return View(model);
+        }
+
+        [HttpGet]
+        [Route("createUserForm")]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateNewUser")]
+        public IActionResult CreateUser(UserModel model) // localhost:[PORT]/user/index
+        {
+            UserModel user = new UserModel()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Id = StaticDb.Users.Count + 1
+            };
+            StaticDb.Users.Add(user);
+            // localhost:[Port]/user/index
+            return RedirectToAction("Index");
+            
+        }
+    }
+}
+
+// 
---
+++ g1/Class_02/SEDC.CLASS_02.Controlers/SEDC.CLASS_02.Controlers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SEDC.CLASS_02.Controlers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
