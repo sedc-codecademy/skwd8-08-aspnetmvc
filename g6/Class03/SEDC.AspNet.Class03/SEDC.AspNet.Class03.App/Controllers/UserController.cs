8af590df6febe6714af484716262d114c502529e
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-08-31 18:57:36 +0200

    added class 03

---
+++ g6/Class03/SEDC.AspNet.Class03/SEDC.AspNet.Class03.App/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.AspNet.Class03.App.Database;
+using SEDC.AspNet.Class03.App.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.AspNet.Class03.App.Controllers
+{
+    [Route("profile")]
+    public class UserController : Controller
+    {
+        [HttpGet("{id:int}")]
+        public IActionResult GetProfile(int id)
+        {
+            var user = PizzaDatabase.Users.FirstOrDefault(u => u.Id == id);
+
+            var address = PizzaDatabase.Addresses.FirstOrDefault(a => a.UserId == user.Id);
+            var subs = PizzaDatabase.NewsletterSubscription.FirstOrDefault(n => n.UserId == user.Id);
+
+            var userDetails = new UserDetailsVM
+            {
+                Id = user.Id,
+                FullName = string.Format("{0} {1}", user.FirstName, user.LastName),
+                Phone = user.Phone,
+                Address = address.Name,
+                IsSubscribed = subs.IsSubscribed ? "Yes" : "No"
+            };
+
+            return View(userDetails);
+        }
+    }
+}
---
+++ g6/Class03/SEDC.AspNet.Class03/SEDC.AspNet.Class03.App/Models/ViewModels/UserDetailsVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.AspNet.Class03.App.Models.ViewModels
+{
+    public class UserDetailsVM
+    {
+        public int Id { get; set; }
+        
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Phone")]
+        public long Phone { get; set; }
+
+        [Display(Name = "Active subscription")]
+        public string IsSubscribed { get; set; }
+    }
+}
