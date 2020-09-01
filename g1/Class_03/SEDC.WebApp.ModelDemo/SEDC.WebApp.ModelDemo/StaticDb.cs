61c07c46ee17eea55c01dfdb015e741987d08035
Goce Kabov <Goce.Kabov@seavus.com>
2020-09-01 21:25:42 +0200

    added class 03

---
+++ g1/Class_03/SEDC.WebApp.ModelDemo/SEDC.WebApp.ModelDemo/StaticDb.cs
@@ -0,0 +1,64 @@
+﻿using SEDC.WebApp.ModelDemo.Models.Domain;
+using SEDC.WebApp.ModelDemo.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.WebApp.ModelDemo
+{
+    public static class StaticDb 
+    {
+        public static List<Order> Orders;
+        public static List<Pizza> Menu;
+        public static List<User> Users;
+        static StaticDb()
+        {
+            Users = new List<User>()
+            {
+                new User()
+                {
+                    Id = 1,
+                    FirstName = "Bob",
+                    LastName = "Bobsky",
+                    Address = "Bob Street",
+                    Phone = 080012312345
+                }
+            };
+            Menu = new List<Pizza>()
+            {
+                new Pizza()
+                {
+                    Id = 1,
+                    Name = "Kapri",
+                    Price = 7,
+                    Size = PizzaSize.Small
+                },
+                new Pizza()
+                {
+                    Id = 2,
+                    Name = "Kapri",
+                    Price = 8,
+                    Size = PizzaSize.Medium
+                },
+                new Pizza()
+                {
+                    Id = 3,
+                    Name = "Kapri",
+                    Price = 9,
+                    Size = PizzaSize.Family
+                }
+            };
+            Orders = new List<Order>()
+            {
+                new Order()
+                {
+                    Id = 1,
+                    User = Users[0],
+                    Pizza = Menu[0],
+                    Delivered = false
+                }
+            };
+        }
+    }
+}
