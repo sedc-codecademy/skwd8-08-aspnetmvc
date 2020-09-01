feea854269ff73c67a65eaa704d4480bccfa3140
Risto Panchevski <Risto.Pancevski@stas.be>
2020-09-01 20:46:22 +0200

    Class 03

---
+++ g5/Class03/Shop/Shop/Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Vnesi ime!")]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 100, ErrorMessage = "Ja pretera!")]
+        public int Quantity { get; set; }
+        public ProductCategory Category { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        [DisplayName("Produced by")]
+        public string ProducedBy { get; set; }
+        [Required]
+        [DisplayName("Image")]
+        public string ImageUrl { get; set; }
+
+        public Product(string name, int quantity, ProductCategory category, decimal price, string producedBy,
+            string imageUrl)
+        {
+            var rnd = new Random();
+            Id = rnd.Next(1, 1000000000);
+            Name = name;
+            Quantity = quantity;
+            Category = category;
+            Price = price;
+            ProducedBy = producedBy;
+            ImageUrl = imageUrl;
+        }
+
+        public Product()
+        {
+            
+        }
+    }
+}
---
+++ g5/Class03/Shop/Shop/Models/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Shop.Models
+{
+    public enum ProductCategory
+    {
+        Drinks = 1,
+        Meat,
+        Vegetables,
+        Fruits
+    }
+}
