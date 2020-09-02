30401e8c02d66c7af8aeca0f4ed71e301d9b0f7d
ivletest <karakan@protonmail.com>
2020-09-02 21:07:07 +0200

    Uploaded class 4

---
+++ g7/Class 4/PizzaApp/PizzaApp.Web/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PizzaApp.Web.Models.Domain;
+using PizzaApp.Web.Models.Enums;
+using PizzaApp.Web.Models.ViewModels;
+
+namespace PizzaApp.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        public User User { get; set; }
+
+        public List<Pizza> Pizzas { get; set; } = new List<Pizza>
+        {
+            new Pizza{ Id = 1, Name = "Pizza 1"},
+            new Pizza{ Id = 2, Name = "Pizza 2"},
+            new Pizza{ Id = 3, Name = "Pizza 3"},
+            new Pizza{ Id = 4, Name = "Pizza 4"},
+        };
+
+        List<OrderViewModel> Orders { get; set; } = new List<OrderViewModel>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Wlcome to Pizza-App orders";
+
+            return View("Order", Orders);
+        }
+
+        [HttpGet]
+        public IActionResult PlaceOrder()
+        {
+            ViewBag.Title = "Place an order";
+
+            ViewData["Pizzas"] = Pizzas;
+
+            ViewData["PizzaSize"] = Enum.GetValues(typeof(PizzaSize)).Cast<PizzaSize>().ToList();
+
+            return View();
+        }
+    }
+}
