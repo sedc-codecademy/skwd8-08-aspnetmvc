8426f8ae649dc1eaa228965a61c698369fa6d740
Goce Kabov <Goce.Kabov@seavus.com>
2020-09-05 13:15:57 +0200

    added class 05

--- g1/Class_05/SEDC.WebApp.ModelDemo/SEDC.WebApp.ModelDemo/Models/Domain/Pizza.cs
+++ g1/Class_05/SEDC.WebApp.ModelDemo/SEDC.WebApp.ModelDemo/Models/Domain/Pizza.cs
@@ -1,18 +1,15 @@
 ﻿using SEDC.WebApp.ModelDemo.Models.Enums;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SEDC.WebApp.ModelDemo.Models.ViewModels
+namespace SEDC.WebApp.ModelDemo.Models.Domain
 {
-    public class PizzaVM
+    public class Pizza
     {
         public int Id { get; set; }
-        [Display(Name= "Pizza Name")]
         public string Name { get; set; }
-        [Display(Name = "PizzaSize")]
         public PizzaSize Size { get; set; }
         public double Price { get; set; }
         public string ImageUrl { get; set; }
