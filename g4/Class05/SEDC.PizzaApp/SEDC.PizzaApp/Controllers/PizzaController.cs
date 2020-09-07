dc7a1eb044eab9984498777f7a2db3f2eb7a97ce
tstojanovska <stojanovskatanja03@gmail.com>
2020-09-07 21:32:29 +0200

    Class 05

---
+++ g4/Class05/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+using SEDC.PizzaApp.Models.Domain;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // returns ViewResult
+        }
+
+        public IActionResult JsonData()
+        {
+            Pizza pizza = new Pizza
+            {
+                Id = 1,
+                Name = "Capri"
+            };
+            return new JsonResult(pizza); // returns JsonResult
+        }
+
+        public IActionResult BackToHome()
+        {
+            return RedirectToAction("Index", "Home"); //redirects to Action Index in Home Controller
+        }
+
+        public IActionResult Details(int? id) // localhost:port/Pizza/Details/1 or  localhost:port/Pizza/Details
+        {
+            if (id != null)
+            {
+                return View();
+            }
+            //  return new EmptyResult();
+            return View("BadRequest");
+        }
+    }
+}
---
+++ g4/Class05/SEDC.PizzaApp/SEDC.PizzaApp/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using SEDC.PizzaApp.Models.Enums;
+
+namespace SEDC.PizzaApp.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public string PizzaStore { get; set; }
+        public bool Delivered { get; set; }
+        [Display(Name="User")]
+        public int UserId { get; set; }
+        public string PizzaName { get; set; }
+    }
+}
