c09a6240313f5ebdc58c644125443591ac7e21e9
Martin Panovski <panovski62@gmail.com>
2020-09-09 21:19:40 +0200

    Class 05 demo code added

---
+++ g2/Class05/Class05.HTMLHelpersAndTags/Class05.ViewsPart2/Controllers/BookController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Class05.ViewsPart2.Helpers;
+using Class05.ViewsPart2.Models.Domain;
+using Class05.ViewsPart2.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class05.ViewsPart2.Controllers
+{
+    public class BookController : Controller
+    {
+        [Route("all-books")]
+        public IActionResult Index()
+        {
+            List<Book> books = StaticDB.Books;
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            BookCreateViewModel model = new BookCreateViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookCreateViewModel model)
+        {
+            Book book = new Book();
+            Author author = new Author();
+
+            int id = StaticDB.Books.Count() + 1;
+
+            if(model != null)
+            {
+                book.Id = id;
+                book.ISBN = model.ISBN;
+                book.Title = model.Title;
+                book.Genre = model.Genre;
+
+                author.FullName = model.AuthorFullName;
+                author.Age = model.AuthorAge.HasValue ? model.AuthorAge.Value : 0;
+
+                book.Author = author;
+                book.IsAvailable = model.IsAvailable;
+
+                StaticDB.Books.Add(book);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            Book book = StaticDB.Books.SingleOrDefault(x => x.Id == id);
+
+            BookDetailsViewModel bookDetails = new BookDetailsViewModel()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorFullName = book.Author.FullName,
+                Genre = book.Genre
+            };
+            return View(bookDetails);
+        }
+    }
+}
