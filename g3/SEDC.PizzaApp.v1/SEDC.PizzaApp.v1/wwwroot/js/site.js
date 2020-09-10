8f0c1a1260c83e5d43deef30f0c192853ed87e6e
Viktor Jakovlev <vicebad@hotmail.com>
2020-09-10 21:16:44 +0200

    Class 06 finished

--- g3/SEDC.PizzaApp.v1/SEDC.PizzaApp.v1/wwwroot/js/site.js
+++ g3/SEDC.PizzaApp.v1/SEDC.PizzaApp.v1/wwwroot/js/site.js
@@ -1,4 +1,11 @@
 ﻿$(document).ready(function () {
+
     //navbar active links
     $(".navbar-nav").find('[href="' + window.location.pathname + '"]').parent().addClass("active");
+
+    //carousel options
+    $('.carousel').carousel({
+        interval: 3000
+    })
+
 })
