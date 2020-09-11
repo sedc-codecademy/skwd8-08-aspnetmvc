bd1537261cd404d0f001a25bc460a9338c468013
Martin Panovski <panovski62@gmail.com>
2020-09-11 21:16:26 +0200

    Class 06 demo code added

--- g2/PizzaApp.v1/SEDC.PizzaApp.v1/SEDC.PizzaApp.v1/Controllers/OrderController.cs
+++ g2/PizzaApp.v1/SEDC.PizzaApp.v1/SEDC.PizzaApp.v1/Controllers/OrderController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.v1.DB;
+using SEDC.PizzaApp.v1.Models.Domain;
 using SEDC.PizzaApp.v1.Models.ViewModels;
 
 namespace SEDC.PizzaApp.v1.Controllers
@@ -12,7 +14,16 @@ namespace SEDC.PizzaApp.v1.Controllers
         [Route("Orders")]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Title = "Welcome to the Orders page!";
+            Order firstOrder = StaticDb.Orders.FirstOrDefault();
+            OrderListViewModel ordersViewModel = new OrderListViewModel()
+            {
+                FirstPizza = firstOrder.Pizza.Name,
+                NumberOfOrders = StaticDb.Orders.Count,
+                FirstCustomerName = $"{firstOrder.User.FirstName} {firstOrder.User.LastName}",
+                Orders = StaticDb.Orders
+            };
+            return View(ordersViewModel);
         }
 
         public IActionResult Details(int? id)
@@ -21,15 +32,45 @@ namespace SEDC.PizzaApp.v1.Controllers
         }
 
 
-        public IActionResult Create()
+        public IActionResult Create(string error)
         {
-            return View();
+            ViewBag.Error = error == null ? "" : error;
+            OrderViewModel model = new OrderViewModel();
+            return View(model);
         }
 
         [HttpPost]
         public IActionResult Create(OrderViewModel model)
         {
-            return View();
+            //First check if there is a pizza like the user entered in the form - by size and name
+            Pizza pizza = StaticDb.Menu.FirstOrDefault(x => x.Name == model.PizzaName && x.Size == model.Size);
+
+            //If there is no such pizza in the database, return an error message to the user
+            if (pizza == null)
+                return RedirectToAction("Create", new { error = "There is no such pizza in the menu!" });
+
+            StaticDb.UserId++;
+            User user = new User()
+            {
+                Id = StaticDb.UserId,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Address = model.Address,
+                Phone = model.Phone,
+            };
+            StaticDb.OrderId++;
+            Order order = new Order()
+            {
+                Id = StaticDb.OrderId,
+                Delivered = false,
+                Pizza = pizza,
+                Price = pizza.Price + 2,
+                User = user
+            };
+
+            StaticDb.Users.Add(user);
+            StaticDb.Orders.Add(order);
+            return View("_ThankYou");
         }
     }
 }
---
+++ g2/PizzaApp.v1/SEDC.PizzaApp.v1/SEDC.PizzaApp.v1/Models/ViewModels/OrderListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SEDC.PizzaApp.v1.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.v1.Models.ViewModels
+{
+    public class OrderListViewModel
+    {
+        public string FirstPizza { get; set; }
+        public int NumberOfOrders{ get; set; }
+        public string FirstCustomerName { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
