29a64642bd820597c00b129a8ee32c848ba9709e
tstojanovska <stojanovskatanja03@gmail.com>
2020-09-12 13:25:07 +0200

    Class 07

---
+++ g4/Class07/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Domain/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SEDC.PizzaApp.Domain.Enums;
+
+namespace SEDC.PizzaApp.Domain.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public bool Delivered { get; set; }
+        public string PizzaStore { get; set; }
+        public List<PizzaOrder> PizzaOrders { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ g4/Class07/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.ViewModels/Order/PizzaOrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SEDC.PizzaApp.Domain.Enums;
+
+namespace SEDC.PizzaApp.ViewModels.Order
+{
+    public class PizzaOrderViewModel
+    {
+        public int OrderId { get; set; }
+        public int PizzaId { get; set; }
+        public PizzaSize PizzaSize { get; set; }
+        public double Price { get; set; }
+    }
+}
