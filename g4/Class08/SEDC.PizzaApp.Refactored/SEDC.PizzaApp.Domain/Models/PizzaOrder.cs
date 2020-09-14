a283c8fd08ca987538a179aeee2ef9ec38bef9e7
tstojanovska <stojanovskatanja03@gmail.com>
2020-09-14 20:57:54 +0200

    Class 08

---
+++ g4/Class08/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Domain/Models/PizzaOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SEDC.PizzaApp.Domain.Enums;
+
+namespace SEDC.PizzaApp.Domain.Models
+{
+    public class PizzaOrder
+    {
+        public int Id { get; set; }
+        public int PizzaId { get; set; }
+        public Pizza Pizza { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public PizzaSize PizzaSize { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ g4/Class08/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SEDC.PizzaApp.ViewModels.User;
+
+namespace SEDC.PizzaApp.Services.Interfaces
+{
+    public interface IUserService
+    {
+        List<UserDDViewModel> GetUsersForDropDown();
+    }
+}
