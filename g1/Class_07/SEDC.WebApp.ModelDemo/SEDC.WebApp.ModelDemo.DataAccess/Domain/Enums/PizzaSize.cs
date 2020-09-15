62310a496335f086e3e37611c72ddc0bf4fa08ef
Goce Kabov <Goce.Kabov@seavus.com>
2020-09-15 21:26:59 +0200

    modified class 07

---
+++ g1/Class_07/SEDC.WebApp.ModelDemo/SEDC.WebApp.ModelDemo.DataAccess/Domain/Enums/PizzaSize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.WebApp.ModelDemo.DataAccess.Domain.Enums
+{
+    public enum PizzaSize
+    {
+        Small = 1,
+        Medium = 2,
+        Family = 3
+    }
+}
