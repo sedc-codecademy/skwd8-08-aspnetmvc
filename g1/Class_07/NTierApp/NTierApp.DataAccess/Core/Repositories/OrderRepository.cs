c84e4593eddc9d53320698730066a8466d818d6a
Goce Kabov <Goce.Kabov@seavus.com>
2020-09-15 19:41:42 +0200

    added class 07

---
+++ g1/Class_07/NTierApp/NTierApp.DataAccess/Core/Repositories/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using NTierApp.DataAccess.Core.Entities;
+using NTierApp.DataAccess.Core.Interfaces;
+using System.Collections.Generic;
+
+namespace NTierApp.DataAccess.Core.Repositories
+{
+    public class OrderRepository : IRepository<Order>
+    {
+        public bool Create(Order entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(Order entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Order> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Order GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(Order entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
