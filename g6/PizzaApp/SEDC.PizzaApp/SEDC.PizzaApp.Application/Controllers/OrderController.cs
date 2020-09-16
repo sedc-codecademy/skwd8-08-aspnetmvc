d173c63c448f7c0aff048339dc22c4c75644b30c
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-09-16 20:53:46 +0200

    Adde part 2.5

---
+++ g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.Application/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.BusinessModels.newModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Application.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Order(string error, int pizzas)
+        {
+            if(error != null)
+            {
+                return View("_Error");
+            }
+
+            var orderVM = new OrderViewModel();
+            orderVM.Pizzas = new List<PizzaViewModelNew>();
+            for (int i = 0; i < pizzas; i++)
+            {
+                orderVM.Pizzas.Add(new PizzaViewModelNew());
+            }
+
+            return View(orderVM);
+        }
+    }
+}
