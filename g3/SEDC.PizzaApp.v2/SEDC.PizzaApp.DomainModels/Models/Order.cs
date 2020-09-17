9949ecd60fb81677cabd15dc062897d61567a8c4
Viktor Jakovlev <vicebad@hotmail.com>
2020-09-17 18:11:19 +0200

    pizza app v2 domain models added

---
+++ g3/SEDC.PizzaApp.v2/SEDC.PizzaApp.DomainModels/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PizzaApp.DomainModels.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+        public double Price { get; set; }
+        public User User { get; set; }
+        public bool IsDelivered { get; set; }
+    }
+}
---
+++ g3/SEDC.PizzaApp.v2/SEDC.PizzaApp.DomainModels/Models/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using SEDC.PizzaApp.DomainModels.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PizzaApp.DomainModels.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public PizzaSize Size { get; set; }
+    }
+}
