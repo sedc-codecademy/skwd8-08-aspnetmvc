28cf634fa25af4a39d6d754e79fa59ce1078f5bd
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-09-19 09:55:26 +0200

    Added part 1 models

---
+++ g6/Class09/SEDC.AspNet.Class09/SEDC.AspNet.Class09.EfCodeFirst/Models/DomainModels/Movie.cs
@@ -0,0 +1,25 @@
+﻿using SEDC.AspNet.Class09.EfCodeFirst.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.AspNet.Class09.EfCodeFirst.Models.DomainModels
+{
+    public class Movie
+    {
+        // table columns
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public string Language { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Length { get; set; }
+        public int AgeRestriction { get; set; }
+        public int Quantity { get; set; }
+
+        // navigation properties
+        public RentalInfo RentalInfo { get; set; }
+    }
+}
---
+++ g6/Class09/SEDC.AspNet.Class09/SEDC.AspNet.Class09.EfCodeFirst/Models/DomainModels/RentalInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.AspNet.Class09.EfCodeFirst.Models.DomainModels
+{
+    public class RentalInfo
+    {
+        // table columns
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateRented { get; set; }
+        public DateTime DateReturned { get; set; }
+
+        // navigation properties
+        public Movie Movie { get; set; }
+        public User User { get; set; }
+    }
+}
