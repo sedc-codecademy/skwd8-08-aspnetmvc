ce7fc11a23f28ee1fc839bc3893bd8ca0ffe5427
Risto Panchevski <Risto.Pancevski@stas.be>
2020-09-19 11:52:13 +0200

    complete architecture

---
+++ g5/PizzaApp - Architecture/PizzaApp/Services/Interfaces/IOrderItemService.cs
@@ -0,0 +1,12 @@
+﻿using ViewModels;
+
+namespace Services.Interfaces
+{
+    public interface IOrderItemService
+    {
+        OrderItemViewModel CreateNewOrderItem(int orderId);
+        void Save(OrderItemViewModel orderItem);
+        void Delete(int id);
+        OrderItemViewModel GetById(int id);
+    }
+}
