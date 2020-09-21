475697f945ff669b09e74a60d90299437fae1a45
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-09-21 20:10:05 +0200

    added pasrt 3

--- g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.BusinessLayer/Helpers/DiModule.cs
+++ g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.BusinessLayer/Helpers/DiModule.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SEDC.PizzaApp.DataAccess;
 using SEDC.PizzaApp.DataAccess.Repositories;
 using SEDC.PizzaApp.DataAccess.Repositories.CacheRepositories;
 using SEDC.PizzaApp.Domain.Models;
@@ -9,10 +11,29 @@ namespace SEDC.PizzaApp.BusinessLayer.Helpers
     {
         public static IServiceCollection RegisterRepositories(IServiceCollection services)
         {
+            // ONE WAY OF REGISTERING DB CONTEXT
+            //var connectionString = "Server=(LocalDb)\\MSSqlLocalDb;Database=PizzaDb;Trusted_Connection=True";
+            //services.AddDbContext<PizzaAppContext>(options =>
+            //{
+            //    options.UseSqlServer(connectionString);
+            //});
+
+            //"Server=(LocalDb)\\MSSqlLocalDb;Database=PizzaDb;Trusted_Connection=True"
+
             services.AddTransient<IRepository<User>, UserRepository>();
             services.AddTransient<IRepository<Order>, OrderRepository>();
             services.AddTransient<IRepository<Pizza>, PizzaRepository>();
             services.AddTransient<IRepository<Feedback>, FeedbackRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection RegisterDbContext(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<PizzaAppContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
 
             return services;
         }
--- g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.Domain/Models/PizzaOrder.cs
+++ g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.Domain/Models/PizzaOrder.cs
@@ -1,7 +1,12 @@
-﻿namespace SEDC.PizzaApp.Domain.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SEDC.PizzaApp.Domain.Models
 {
     public class PizzaOrder
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int OrderId { get; set; }
         public Order Order { get; set; }
