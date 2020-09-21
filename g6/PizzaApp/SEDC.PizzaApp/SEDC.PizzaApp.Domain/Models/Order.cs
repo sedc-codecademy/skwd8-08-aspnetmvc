bcec96db9806048f50eefaa04c8ca9497e0bda41
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2020-09-21 19:49:17 +0200

    added part 2

--- g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.Domain/Models/Order.cs
+++ g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.Domain/Models/Order.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 
 namespace SEDC.PizzaApp.Domain.Models
 {
     public class Order
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public User User { get; set; }
         public int UserId { get; set; }
--- g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.Domain/Models/User.cs
+++ g6/PizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.Domain/Models/User.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SEDC.PizzaApp.Domain.Models
 {
     public class User
     {
+        // other way of configuring PK
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
