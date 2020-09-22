c881479ea0fe13ba1f68d0fef7c03cfcb0e06d8d
viktorjakovlev <viktorjakovlev@hotmail.com>
2020-09-22 18:56:38 +0200

    class 10

---
+++ g3/Class10/SimpleAppWithEntity/SimpleAppWithEntity.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleAppWithEntity.ViewModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleAppWithEntity.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddName() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddName(NameViewModel model)
+        {
+            return new EmptyResult();
+        }
+
+    }
+}
