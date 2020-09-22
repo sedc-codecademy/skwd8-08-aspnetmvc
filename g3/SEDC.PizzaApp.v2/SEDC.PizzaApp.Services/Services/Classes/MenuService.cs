2c17073bb21eedf8e8ab039548b313600c2c895b
viktorjakovlev <viktorjakovlev@hotmail.com>
2020-09-22 18:19:48 +0200

    updates

--- g3/SEDC.PizzaApp.v2/SEDC.PizzaApp.Services/Services/Classes/MenuService.cs
+++ g3/SEDC.PizzaApp.v2/SEDC.PizzaApp.Services/Services/Classes/MenuService.cs
@@ -3,6 +3,7 @@ using SEDC.PizzaApp.DataAccess.Repositories.CashRepositories;
 using SEDC.PizzaApp.DomainModels.Enums;
 using SEDC.PizzaApp.DomainModels.Models;
 using SEDC.PizzaApp.Services.Services.Interfaces;
+using SEDC.PizzaApp.ViewModels.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +28,22 @@ namespace SEDC.PizzaApp.Services.Services.Classes
         public Pizza GetPizzaByNameAndSize(string name, PizzaSize size) 
         {
             return _pizzaRepository.GetAll().FirstOrDefault(x => x.Name == name && x.Size == size);
+        }
+
+        public int AddPizzaInMenu(AddPizzaViewModel model) 
+        {
+            var LastPizzaId = GetMenu().Last().Id;
+
+            var pizza = new Pizza()
+            {
+                Id = LastPizzaId + 1,
+                Name = model.Name,
+                Price = model.Price,
+                Size = model.Size
+            };
+
+            var response = _pizzaRepository.Insert(pizza);
+            return response;
         }
     }
 }
